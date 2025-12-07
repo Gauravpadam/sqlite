b4dd5f1a2d138007f2e8595e3ea75dc6e7270169
Gauravpadam <gauravpadam2@gmail.com>
2025-12-08 00:01:26 +0530

    3.Adding unit tests via rspec and hardening of logic using TDD

--- repl.c
+++ repl.c
@@ -9,8 +9,8 @@
 
 typedef struct {
     uint32_t id;
-    char username[COLUMN_USERNAME_SIZE];
-    char email[COLUMN_EMAIL_SIZE];
+    char username[COLUMN_USERNAME_SIZE + 1]; // 1 reserved space for the null character
+    char email[COLUMN_EMAIL_SIZE + 1]; // 1 reserved space for the null character
 } Row;
 
 const uint32_t ID_SIZE = size_of_attribute(Row, id);
@@ -83,7 +83,9 @@ typedef enum {
 
 typedef enum {
     PREPARE_SUCCESS,
+    PREPARE_NEGATIVE_ID,
     PREPARE_SYNTAX_ERROR,
+    PREPARE_STRING_TOO_LONG,
     PREPARE_UNRECOGNIZED_STATEMENT
 } PrepareResult;
 
@@ -137,17 +139,45 @@ typedef struct{
     Row row_to_insert; // only used by insert statement
 } Statement;
 
+PrepareResult prepare_insert(InputBuffer* input_buffer, Statement* statement){
+
+    statement->type = STATEMENT_INSERT;
+
+    char* keyword = strtok(input_buffer->buffer, " ");
+    char* id_string = strtok(NULL, " ");
+    char* username = strtok(NULL, " ");
+    char* email = strtok(NULL, " ");
+
+    if (id_string == NULL || username == NULL || email == NULL){
+        return PREPARE_SYNTAX_ERROR;
+    }
+
+    int id = atoi(id_string);
+    if (id < 0){
+        return PREPARE_NEGATIVE_ID;
+    }
+    if (strlen(username) > COLUMN_USERNAME_SIZE){
+        return PREPARE_STRING_TOO_LONG;
+    }
+    if (strlen(email) > COLUMN_EMAIL_SIZE){
+        return PREPARE_STRING_TOO_LONG;
+    }
+
+    statement->row_to_insert.id = id;
+    strcpy(statement->row_to_insert.username, username);
+    strcpy(statement->row_to_insert.email, email);
+
+    return PREPARE_SUCCESS;
+}
+
+
 PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement){
     if (strncmp(input_buffer->buffer, "insert", 6) == 0){
-        statement->type = STATEMENT_INSERT;
-        int args_assigned = sscanf(
-            input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id),
-            statement->row_to_insert.username, statement->row_to_insert.email);
-        
-        if (args_assigned < 3){
-            return PREPARE_SYNTAX_ERROR;
-        }
-        return PREPARE_SUCCESS;
+        return prepare_insert(input_buffer, statement);
+
+        // if ((strlen(statement->row_to_insert.username) > COLUMN_USERNAME_SIZE + 1) || (strlen(statement->row_to_insert.username) > COLUMN_EMAIL_SIZE + 1)){
+
+        // }
     }
     if (strncmp(input_buffer->buffer, "select", 6) == 0){
         statement->type = STATEMENT_SELECT;
@@ -238,6 +268,12 @@ int main(int argc, char* argv[]){
             case(PREPARE_SYNTAX_ERROR):
                 printf("Syntax error, cannot parse statement. \n");
                 continue;
+            case (PREPARE_STRING_TOO_LONG):
+                printf("String is too long.\n");
+                continue;
+            case (PREPARE_NEGATIVE_ID):
+                printf("ID must be positive.\n");
+                continue;
             case (PREPARE_UNRECOGNIZED_STATEMENT):
                 printf("Unrecognized keyword at start of '%s' \n", input_buffer->buffer);
                 continue;
@@ -245,10 +281,10 @@ int main(int argc, char* argv[]){
 
         switch(execute_statement(&statement, table)){
             case (EXECUTE_SUCCESS):
-                printf("Executed. \n");
+                printf("Executed.\n");
                 break;
             case (EXECUTE_TABLE_FULL):
-                printf("Error: Table full. \n");
+                printf("Error: Table full.\n");
                 break;
         }
 
